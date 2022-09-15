b451970f724686b133a0c6fbfda83100dcc64942
Md. Almas Ali <almaspr3@gmail.com>
2022-09-16 01:34:45 +0600

    Updated get settings.

--- bright.py
+++ bright.py
@@ -1,3 +1,6 @@
+# usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 '''
 A Brightness control utility for Linux systems.
 
@@ -5,17 +8,30 @@ Easy to use it
 '''
 
 from os import system as cmd
+import subprocess
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--set', help='Set brightness level (1-100)')
+parser.add_argument('--set', help='Set brightness level (1-100+)', type=int)
+parser.add_argument(
+    '--get', help='Get brightness level (1-100+)', action='store_true')
 
 args = parser.parse_args()
 
 if args.set:
     bright = int(args.set)/100
     cmd(f'xrandr --output LVDS-1 --brightness {bright}')
+elif args.get:
+    # br = subprocess.Popen(
+    #     "xrandr", "--verbose | awk '/Brightness/ { print $2; exit }'")
+    # print("brightness: ", br.stdout.read())
+
+    process = subprocess.Popen(["xrandr", "--verbose", "|", "awk", "'/Brightness/ { print $2; exit }'"],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout)
+    print(stderr)
 else:
     parser.print_help()
 
